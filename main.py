e8a1fcf53d2525e815bc97c48ae08d9566512992
MSeidl93 <matthias.seidl@gmx.de>
2024-03-13 14:34:59 +0100

    added logo

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def fetch_data():
     df = pd.DataFrame(data)
     
     # Filter based on conditions
-    filtered_df = df[(df['metric_key'] == 'txcosts_median_usd') & (df['granularity'] == 'hourly')]
+    filtered_df = df[(df['granularity'] == 'hourly')]
     
     # Convert unix timestamp to datetime for better readability on plot
     filtered_df['datetime'] = pd.to_datetime(filtered_df['unix'], unit='ms')
@@ -27,28 +27,30 @@ def fetch_data():
 def plot_data(df):
     ## rename column datetime to "Date" and value to "Median Transaction Costs in USD"
     df = df.copy()
+    df = df[(df['metric_key'] == 'txcosts_median_usd')]
     df.rename(columns={'datetime': 'Date', 'value': 'Median Transaction Costs in USD', 'origin_key' : 'Chain'}, inplace=True)
 
     st.line_chart(df, x='Date', y='Median Transaction Costs in USD', color='Chain')
 
-def create_table(df):
-    ## order by unix desc and only keep latest value per origin_key
-    df = df.sort_values('unix', ascending=False).drop_duplicates('origin_key')
-    df = df[['origin_key', 'value', 'datetime']]
-    df.set_index('origin_key', inplace=True)
+# def create_table(df):
+#     ## order by unix desc and only keep latest value per origin_key
+#     df = df.sort_values('unix', ascending=False).drop_duplicates('origin_key')
+#     df = df[['origin_key', 'value', 'datetime']]
+#     df.set_index('origin_key', inplace=True)
 
-    #order by value ascending
-    df = df.sort_values('value', ascending=True)
+#     #order by value ascending
+#     df = df.sort_values('value', ascending=True)
 
-    ##value column in USD
-    df['value'] = df['value'].apply(lambda x: f"${x:,.3f}")
+#     ##value column in USD
+#     df['value'] = df['value'].apply(lambda x: f"${x:,.3f}")
 
-    ## rename column value to "Median Transaction Costs in USD" and datetime to "Last Updated"
-    df.columns = ["Median Transaction Costs in USD", "Last Updated (UTC)"]
+#     ## rename column value to "Median Transaction Costs in USD" and datetime to "Last Updated"
+#     df.columns = ["Median Transaction Costs", "Last Updated (UTC)"]
 
-    st.table(df)
+#     st.table(df)
 
 def main():
+    st.image('gtp-logo-on-white.png', caption='growthepie logo')
     # Check if 5 minutes have passed since the last API call
     current_time = time.time()
     if current_time - st.session_state['last_run'] > 300:  # 5 minutes in seconds
@@ -61,7 +63,7 @@ def main():
     st.header("Median Transaction Costs in USD")
     st.subheader("Data from growthepie.xyz")
     plot_data(df)
-    create_table(df)
+    # create_table(df)
 
 if __name__ == "__main__":
     main()
