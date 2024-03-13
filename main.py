eae8140a3735ccb32c4c693f50bf7c813da97b5c
MSeidl93 <matthias.seidl@gmx.de>
2024-03-13 21:29:50 +0100

    added timespan

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 import time
+from datetime import datetime
 
 # Initialize or get the last run timestamp
 if 'last_run' not in st.session_state:
@@ -89,6 +90,13 @@ def main():
         ['OP Mainnet', 'Arbitrum', 'Base', 'Zora', 'Starknet', 'Linea', 'zkSync Era'],
         ['OP Mainnet', 'Arbitrum', 'Base', 'Zora', 'Starknet', 'Linea', 'zkSync Era'])
     df = df[df['origin_key'].isin(options)]
+
+    start_time = st.slider(
+        "Timespan",
+        value=datetime(2024, 3, 12, 12, 00),
+        format="MM/DD/YY - hh:mm")
+
+    df = df[df['datetime'] > start_time]
 
     plot_data(df)
     create_table(df)
